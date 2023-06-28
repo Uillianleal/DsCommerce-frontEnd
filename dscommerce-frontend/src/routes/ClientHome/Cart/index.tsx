e238bcbe4e6297acb7336092e12fe6c8184334f5
Uillian Leal <willianleal52@hotmail.com>
2023-06-28 11:36:07 -0300

    increaseItem

--- dscommerce-frontend/src/routes/ClientHome/Cart/index.tsx
+++ dscommerce-frontend/src/routes/ClientHome/Cart/index.tsx
@@ -12,6 +12,11 @@ function Cart() {
     setCart(cartService.getCart());
   }
 
+  function handleIncreaseItem(productId: number) {
+    cartService.increaseItem(productId);
+    setCart(cartService.getCart());
+  }
+
   return (
     <main>
       <section id="cart-container-section" className="dsc-container">
@@ -35,7 +40,12 @@ function Cart() {
                     <div className="dsc-cart-item-quantity-container">
                       <div className="dsc-cart-item-quantity-btn">-</div>
                       <p>{item.quantity}</p>
-                      <div className="dsc-cart-item-quantity-btn">+</div>
+                      <div
+                        onClick={() => handleIncreaseItem(item.productId)}
+                        className="dsc-cart-item-quantity-btn"
+                      >
+                        +
+                      </div>
                     </div>
                   </div>
                 </div>
