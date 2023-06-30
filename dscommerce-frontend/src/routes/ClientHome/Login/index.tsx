07502fa60a08b37fa8ad504936d4e3f6deec5e4c
Uillian Leal <willianleal52@hotmail.com>
2023-06-30 11:03:00 -0300

    Login form, login request

--- dscommerce-frontend/src/routes/ClientHome/Login/index.tsx
+++ dscommerce-frontend/src/routes/ClientHome/Login/index.tsx
@@ -1,15 +1,44 @@
 import "./styles.css";
+import { CredentialsDTO } from "../../../models/auth";
+import { useState } from "react";
+import * as authService from "../../../services/auth-service";
 
 function Login() {
+  const [formData, setFormData] = useState<CredentialsDTO>({
+    username: "",
+    password: "",
+  });
+
+  function handleSubmit(event: any) {
+    event.preventDefault();
+    authService
+      .loginRequest(formData)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log("Erro no login,", error);
+      });
+  }
+
+  function handleInputChange(event: any) {
+    const value = event.target.value;
+    const name = event.target.name;
+    setFormData({ ...formData, [name]: value });
+  }
+
   return (
     <main>
       <section id="login-section" className="dsc-container">
         <div className="dsc-login-form-container">
-          <form className="dsc-card dsc-form">
+          <form className="dsc-card dsc-form" onSubmit={handleSubmit}>
             <h2>Login</h2>
             <div className="dsc-form-controls-container">
               <div>
                 <input
+                  onChange={handleInputChange}
+                  name="username"
+                  value={formData.username}
                   className="dsc-form-control"
                   type="text"
                   placeholder="Email"
@@ -18,6 +47,9 @@ function Login() {
               </div>
               <div>
                 <input
+                  onChange={handleInputChange}
+                  name="password"
+                  value={formData.password}
                   className="dsc-form-control"
                   type="password"
                   placeholder="Senha"
