fbc3133b656beb21e4391382b86f37a477cd039b
Uillian Leal <willianleal52@hotmail.com>
2023-06-30 11:39:13 -0300

    Admin page layout

---
+++ dscommerce-frontend/src/components/HeaderAdmin/index.tsx
@@ -0,0 +1,31 @@
+import "./styles.css";
+import homeIcon from "../../assets/home.svg";
+import stockIcon from "../../assets/stock.svg";
+
+function HeaderAdmin() {
+  return (
+    <header className="dsc-header-admin">
+      <nav className="dsc-container">
+        <h1>DSC Admin</h1>
+        <div className="dsc-navbar-right">
+          <div className="dsc-menu-items-container">
+            <div className="dsc-menu-item">
+              <img src={homeIcon} alt="home" />
+              <p>Início</p>
+            </div>
+            <div className="dsc-menu-item">
+              <img src={stockIcon} alt="Cadastro de produtos" />
+              <p className="dsc-menu-item-active">Produtos</p>
+            </div>
+          </div>
+          <div className="dsc-logged-user">
+            <p>Maria Silva</p>
+            <a href="#">Sair</a>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default HeaderAdmin;
