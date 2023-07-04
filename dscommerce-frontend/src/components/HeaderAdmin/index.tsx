1104d14f665f5a5f92fef71e811d7f81241e4a49
Uillian Leal <willianleal52@hotmail.com>
2023-07-04 16:09:18 -0300

    LoggedUser

--- dscommerce-frontend/src/components/HeaderAdmin/index.tsx
+++ dscommerce-frontend/src/components/HeaderAdmin/index.tsx
@@ -1,6 +1,7 @@
 import "./styles.css";
 import homeIcon from "../../assets/home.svg";
 import stockIcon from "../../assets/stock.svg";
+import LoggedUser from "../LoggedUser";
 
 function HeaderAdmin() {
   return (
@@ -18,10 +19,7 @@ function HeaderAdmin() {
               <p className="dsc-menu-item-active">Produtos</p>
             </div>
           </div>
-          <div className="dsc-logged-user">
-            <p>Maria Silva</p>
-            <a href="#">Sair</a>
-          </div>
+          <LoggedUser />
         </div>
       </nav>
     </header>
