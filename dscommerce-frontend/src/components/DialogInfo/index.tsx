9a8917c69a098f714d7502dd6ca325808a304286
Uillian Leal <willianleal52@hotmail.com>
2023-07-07 09:41:54 -0300

    Dialog modal info

---
+++ dscommerce-frontend/src/components/DialogInfo/index.tsx
@@ -0,0 +1,27 @@
+import ButtonPrimary from "../ButtonPrimary";
+
+type Props = {
+  message: string;
+  onDialogClose: () => void;
+};
+
+function DialogInfo({ message, onDialogClose }: Props) {
+  return (
+    <div className="dsc-dialog-background" onClick={() => onDialogClose()}>
+      <div
+        className="dsc-dialog-box"
+        onClick={(event) => event.stopPropagation()}
+      >
+        <h2>{message}</h2>
+        <div
+          className="dsc-dialog-btn-container"
+          onClick={() => onDialogClose()}
+        >
+          <ButtonPrimary text="Ok" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DialogInfo;
