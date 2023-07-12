8cb3b416a90c06ad940496a8517e4b822e553505
Uillian Leal <willianleal52@hotmail.com>
2023-07-12 13:45:18 -0300

    Dirty and validated all

--- dscommerce-frontend/src/routes/Admin/AdminHome/ProductListing/index.tsx
+++ dscommerce-frontend/src/routes/Admin/AdminHome/ProductListing/index.tsx
@@ -66,6 +66,10 @@ function ProductListing() {
     setDialogInfoData({ ...dialogInfoData, visible: false });
   }
 
+  function handleUpdateClick(productId: number) {
+    navigate(`/admin/products/${productId}`);
+  }
+
   function handleDeleteClick(productId: number) {
     setDialogConfirmationData({
       ...dialogConfirmationData,
@@ -131,6 +135,7 @@ function ProductListing() {
                 <td className="dsc-txt-left">{product.name}</td>
                 <td>
                   <img
+                    onClick={() => handleUpdateClick(product.id)}
                     className="dsc-product-listing-btn"
                     src={editIcon}
                     alt="Editar"
